cc7cc7354d23a59b74c32de0759633487bdebac9
Grzegorz Łapiński <grzegorz.lapinski.2001@gmail.com>
2024-10-13 19:34:55 +0200

    First site build, NavBar in progress

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import "../styles/NavBar.css";
+
+function NavBar() {
+    const [reorderOpen, setReorderOpen] = useState(false);
+
+    const toggleNavBar = () => {
+        setReorderOpen(!reorderOpen);
+    };
+
+    return (
+        <div className="navBar">
+            <div className="leftSide" id={reorderOpen ? "open" : "close"}>
+                <img src={logo}></img>
+                <div className="hiddenLinks">
+                    <Link to="/">Strona Główna</Link>
+                    <Link to="/news">Aktualności</Link>
+                    <Link to="/about">O nas</Link>
+                    <Link to="/contact">Kontakt</Link>
+                </div>
+            </div>
+            <div className="rightSide">
+                <Link to="/">Strona Główna</Link>
+                <Link to="/news">Aktualności</Link>
+                <Link to="/about">O nas</Link>
+                <Link to="/contact">Kontakt</Link>
+                <button onClick={toggleNavBar}>
+                    <ReorderIcon />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+    return <div>HOMEEEE</div>;
+}
+
+export default Home;
