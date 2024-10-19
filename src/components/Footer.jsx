10c93eee5b80cb6313cc20ee600bdf28eef0231b
Grzegorz Łapiński <grzegorz.lapinski.2001@gmail.com>
2024-10-19 16:12:26 +0200

    Add footer ourPagesLinks with css, improve some css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,44 @@
 import React from "react";
+import logo_p1 from "../assets/logo_p1.png";
+import logo_p2 from "../assets/logo_p2.png";
+import logo_pk from "../assets/logo_pk.png";
+import logo_pp from "../assets/logo_pp.png";
+import logo_fpd_bg from "../assets/logo_fpd_bg.png";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import "../styles/Footer.css";
 
 function Footer() {
-    const instagramButton = () => {
-        window.open("https://www.instagram.com/telewizjapuls_official/");
-    };
-
-    const youTubeButton = () => {
-        window.open("https://www.youtube.com/user/PulsTelewizja");
-    };
-
-    const facebookButton = () => {
-        window.open("https://www.facebook.com/tvpulspl");
-    };
-
     return (
         <div className="footer">
-            <div className="socialMedia">
-                <button onClick={instagramButton}>
+            <div className="ourPagesLinks">
+                <a href="//tvpuls.pl">
+                    <img src={logo_p1} alt="TV Puls"></img>
+                </a>
+                <a href="//puls2.pl">
+                    <img src={logo_p2} alt="PULS2"></img>
+                </a>
+                <a href="//pulskids.pl">
+                    <img src={logo_pk} alt="Puls Kids"></img>
+                </a>
+                <a href="//playpuls.pl">
+                    <img src={logo_pp} alt="Play Puls"></img>
+                </a>
+                <a href="//fundacjapoddebem.pl">
+                    <img src={logo_fpd_bg} alt="Fundacja Pod Dębem"></img>
+                </a>
+            </div>
+            <div className="socialMediaLinks">
+                <a href="https://www.instagram.com/telewizjapuls_official/">
                     <InstagramIcon />
-                </button>
-                <button onClick={youTubeButton}>
+                </a>
+                <a href="https://www.youtube.com/user/PulsTelewizja">
                     <YouTubeIcon />
-                </button>
-                <button onClick={facebookButton}>
+                </a>
+                <a href="https://www.facebook.com/tvpulspl">
                     <FacebookIcon />
-                </button>
+                </a>
             </div>
             <p>&copy; Telewizja Puls Sp.z o.o. 2024</p>
         </div>
