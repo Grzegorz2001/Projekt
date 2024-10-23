0df1e9dae0b5cd59e515ea2421c58e827e0f7cd9
Grzegorz Łapiński <grzegorz.lapinski.2001@gmail.com>
2024-10-23 21:38:32 +0200

    Update footer logic and css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo_p1 from "../assets/logo_p1.png";
 import logo_p2 from "../assets/logo_p2.png";
 import logo_pk from "../assets/logo_pk.png";
@@ -8,38 +9,68 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import "../styles/Footer.css";
+import { patch } from "@mui/material";
+
+const ourTVLinks = [
+    {
+        title: "TV Puls",
+        image: <img src={logo_p1} alt="TV Puls"></img>,
+        path: "//tvpuls.pl",
+    },
+    {
+        title: "PULS 2",
+        image: <img src={logo_p2} alt="PULS2"></img>,
+        path: "//puls2.pl",
+    },
+    {
+        title: "Puls Kids",
+        image: <img src={logo_pk} alt="Puls Kids"></img>,
+        path: "//pulskids.pl",
+    },
+    {
+        title: "Play Puls",
+        image: <img src={logo_pp} alt="Play Puls"></img>,
+        path: "//playpuls.pl",
+    },
+    {
+        title: "Fundacja Pod Dębem",
+        image: <img src={logo_fpd_bg} alt="Fundacja Pod Dębem"></img>,
+        path: "//fundacjapoddebem.pl",
+    },
+];
+
+const ourSocialMediaLinks = [
+    {
+        image: <InstagramIcon />,
+        path: "https://www.instagram.com/telewizjapuls_official/",
+    },
+    {
+        image: <YouTubeIcon />,
+        path: "https://www.youtube.com/user/PulsTelewizja",
+    },
+    {
+        image: <FacebookIcon />,
+        path: "https://www.facebook.com/tvpulspl",
+    },
+];
 
 function Footer() {
     return (
         <div className="footer">
-            <div className="ourPagesLinks">
-                <a href="//tvpuls.pl">
-                    <img src={logo_p1} alt="TV Puls"></img>
-                </a>
-                <a href="//puls2.pl">
-                    <img src={logo_p2} alt="PULS2"></img>
-                </a>
-                <a href="//pulskids.pl">
-                    <img src={logo_pk} alt="Puls Kids"></img>
-                </a>
-                <a href="//playpuls.pl">
-                    <img src={logo_pp} alt="Play Puls"></img>
-                </a>
-                <a href="//fundacjapoddebem.pl">
-                    <img src={logo_fpd_bg} alt="Fundacja Pod Dębem"></img>
-                </a>
-            </div>
-            <div className="socialMediaLinks">
-                <a href="https://www.instagram.com/telewizjapuls_official/">
-                    <InstagramIcon />
-                </a>
-                <a href="https://www.youtube.com/user/PulsTelewizja">
-                    <YouTubeIcon />
-                </a>
-                <a href="https://www.facebook.com/tvpulspl">
-                    <FacebookIcon />
-                </a>
-            </div>
+            <ul className="ourPagesLinks">
+                {ourTVLinks.map((link, index) => (
+                    <li key={index}>
+                        <Link to={link.path}>{link.image}</Link>
+                    </li>
+                ))}
+            </ul>
+            <ul className="socialMediaLinks">
+                {ourSocialMediaLinks.map((link, index) => (
+                    <li key={index}>
+                        <Link to={link.path}>{link.image}</Link>
+                    </li>
+                ))}
+            </ul>
             <p>&copy; Telewizja Puls Sp.z o.o. 2024</p>
         </div>
     );
